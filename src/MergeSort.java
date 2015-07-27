a349cdd61cffb292abfd456e66ab0aeabda421a2
Ben Liang <liang355@umn.edu>
2015-07-27 03:56:16 -0500

    update merge sort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -31,31 +31,31 @@ public class MergeSort {
 	}
 	
 	public static void Merge(int[] list, int p, int q, int r){
-			int[] sublistL = Arrays.copyOfRange(list, p, q + 1);
-			int[] sublistR = Arrays.copyOfRange(list, q + 1, r + 1);
-			int i = 0;
-			int j = 0;
-			int k = p;
-				while(i < sublistL.length && j <sublistR.length && k <= r){
-					if(sublistL[i] <= sublistR[j]){
-						list[k] = sublistL[i];
-						i++;
-					}else{
-						list[k] = sublistR[j];
-						j++;
-					}
-					k++;
-				}
-				while(i >= sublistL.length && k <= r){
-					list[k] = sublistR[j];
-					j++;
-					k++;
-				}
-				while(j >= sublistR.length && k <= r){
-					list[k] = sublistL[i];
-					i++;
-					k++;
-				}
+		int[] sublistL = Arrays.copyOfRange(list, p, q + 1);
+		int[] sublistR = Arrays.copyOfRange(list, q + 1, r + 1);
+		int i = 0;
+		int j = 0;
+		int k = p;
+		while(i < sublistL.length && j <sublistR.length && k <= r){
+			if(sublistL[i] <= sublistR[j]){
+				list[k] = sublistL[i];
+				i++;
+			}else{
+				list[k] = sublistR[j];
+				j++;
+			}
+			k++;
+		}
+		while(i >= sublistL.length && k <= r){
+			list[k] = sublistR[j];
+			j++;
+			k++;
+		}
+		while(j >= sublistR.length && k <= r){
+			list[k] = sublistL[i];
+			i++;
+			k++;
+		}
 	}
 	
 	public static void PrintList(String text, int list[]){
